9bdb4128606e5af5fb4bd7fe4643e1105256aaa4
Hosting <163719152+hosting-everyone@users.noreply.github.com>
2024-05-16 11:49:03 +0200

    Create maintenance_page.php

---
+++ maintenance_page.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Onderhoud</title>
+</head>
+<body>
+    <h1>Deze website is momenteel in onderhoud</h1>
+    <p>Probeer het later opnieuw.</p>
+</body>
+</html>
